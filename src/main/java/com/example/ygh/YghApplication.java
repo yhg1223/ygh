6905b36d19b58deb5e85bda49de66bc5c1058acf
yhg1223 <1614476153@qq.com>
2020-06-09 10:41:11 +0800

    提交人：ygh 提交原因：测试

---
+++ src/main/java/com/example/ygh/YghApplication.java
@@ -0,0 +1,17 @@
+package com.example.ygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试githup
+ * 
+ * */
+@SpringBootApplication
+public class YghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YghApplication.class, args);
+    }
+
+}
